2c6c4f8c4a6ef74bc0c7d2e687fd98b29c45d407
brunosj <contact@landozone.net>
2023-10-25 10:37:36 +0200

    add additional semester options

--- src/components/courses/CourseCard.tsx
+++ src/components/courses/CourseCard.tsx
@@ -31,17 +31,40 @@ const CourseCard = ({ item }: Props) => {
       graduate: "2e cycle",
     },
   } as { [key: string]: LevelTranslations };
-
   const semesterTranslation = {
     en: {
+      "winter 2020": "Winter 2020",
+      "summer 2020": "Summer 2020",
+      "fall 2020": "Fall 2020",
+      "winter 2021": "Winter 2021",
+      "summer 2021": "Summer 2021",
+      "fall 2021": "Fall 2021",
+      "winter 2022": "Winter 2022",
+      "summer 2022": "Summer 2022",
+      "fall 2022": "Fall 2022",
       "winter 2023": "Winter 2023",
+      "summer 2023": "Summer 2023",
       "fall 2023": "Fall 2023",
       "winter 2024": "Winter 2024",
+      "summer 2024": "Summer 2024",
+      "fall 2024": "Fall 2024",
     },
     fr: {
+      "winter 2020": "Hiver 2020",
+      "summer 2020": "Été 2020",
+      "fall 2020": "Automne 2020",
+      "winter 2021": "Hiver 2021",
+      "summer 2021": "Été 2021",
+      "fall 2021": "Automne 2021",
+      "winter 2022": "Hiver 2022",
+      "summer 2022": "Été 2022",
+      "fall 2022": "Automne 2022",
       "winter 2023": "Hiver 2023",
+      "summer 2023": "Été 2023",
       "fall 2023": "Automne 2023",
       "winter 2024": "Hiver 2024",
+      "summer 2024": "Été 2024",
+      "fall 2024": "Automne 2024",
     },
   } as { [key: string]: SemesterTranslations };
 
